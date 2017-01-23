b1e815e440669ecaec75b6a32a505acecf44679e
Edwin Goddard <Edwin.Goddard@heatsoftware.com>
2017-01-23 13:45:18 +0000

    Start MVC project with WebUI

---
+++ ManageDB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ManageDB.MvcApplication" Language="C#" %>
---
+++ ManageDB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ManageDB.Startup))]
+namespace ManageDB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
